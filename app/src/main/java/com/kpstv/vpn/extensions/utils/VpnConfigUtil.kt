d057435a7f926c5f85787ace3fe12c86ee89e332
Kaustubh Patange <developerkp16@gmail.com>
2021-08-04 17:06:43 +0530

    [Fixed] ANR due to wrong stream of file

--- app/src/main/java/com/kpstv/vpn/extensions/utils/VpnConfigUtil.kt
+++ app/src/main/java/com/kpstv/vpn/extensions/utils/VpnConfigUtil.kt
@@ -9,20 +9,24 @@ object VpnConfigUtil {
   private val certificateRSARegex = "[-]+BEGIN\\sRSA\\sPRIVATE\\sKEY[-]+".toRegex()
 
   fun verifyConfigData(context: Context, uri: Uri): Boolean {
-    val stream = context.contentResolver.openInputStream(uri) ?: return false
+    try {
+      val stream = context.contentResolver.openInputStream(uri) ?: return false
 
-    val lines = stream.bufferedReader().readLines()
-    stream.close()
+      val lines = stream.bufferedReader().readLines()
+      stream.close()
 
-    // OPEN VPN uses tcp or udp protocol
-    if (!(lines.contains("client") && (lines.contains("proto udp") || lines.contains("proto tcp")))) {
+      // OPEN VPN uses tcp or udp protocol
+      if (!(lines.contains("client") && (lines.contains("proto udp") || lines.contains("proto tcp")))) {
+        return false
+      }
+
+      // check for certificates
+      if (lines.none { it.matches(certificateRegex) }) return false
+      if (lines.none { it.matches(certificateRSARegex) }) return false
+
+      return true
+    } catch (e: Exception) {
       return false
     }
-
-    // check for certificates
-    if (lines.none { it.matches(certificateRegex) }) return false
-    if (lines.none { it.matches(certificateRSARegex) }) return false
-
-    return true
   }
 }
