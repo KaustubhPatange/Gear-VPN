10f7824f3499e7e94c98d9566336bbd036e5e156
KaustubhPatange <developerkp16@gmail.com>
2021-12-23 16:53:21 +0530

    [Added] Feedback or Report button in about screen

--- app/src/main/java/com/kpstv/vpn/ui/screens/About.kt
+++ app/src/main/java/com/kpstv/vpn/ui/screens/About.kt
@@ -2,13 +2,13 @@ package com.kpstv.vpn.ui.screens
 
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,7 +16,10 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,6 +31,7 @@ import com.kpstv.vpn.extensions.utils.AppUtils.launchUrl
 import com.kpstv.vpn.extensions.utils.AppUtils.launchUrlInApp
 import com.kpstv.vpn.ui.components.Header
 import com.kpstv.vpn.ui.components.HeaderButton
+import com.kpstv.vpn.ui.components.ThemeButton
 import com.kpstv.vpn.ui.theme.CommonPreviewTheme
 import com.kpstv.vpn.ui.theme.dotColor
 import com.kpstv.vpn.ui.theme.foregroundColor
@@ -115,7 +119,69 @@ fun AboutScreen(goBack: () -> Unit) {
         }
       }
     }
+
+    Column(
+      modifier = Modifier
+        .align(Alignment.BottomCenter)
+        .navigationBarsPadding()
+        .fillMaxWidth(),
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      ThemeButton(
+        onClick = {
+          context.launchUrl(context.getString(R.string.app_github_issues_new))
+        },
+        text = stringResource(R.string.report_button)
+      )
+      Spacer(modifier = Modifier.height(7.dp))
+      FeedbackHelpText()
+    }
   }
+
+}
+
+@Composable
+private fun FeedbackHelpText(modifier: Modifier = Modifier) {
+  val context = LocalContext.current
+
+  val annotatedString = buildAnnotatedString {
+    val mainString = stringResource(R.string.report_message)
+    append(mainString)
+    val urlData = mainString.split("\\s+".toRegex()).find { it.contains(".com") } ?: ""
+
+    val startIndex = mainString.indexOf(urlData)
+    val endIndex = startIndex + urlData.length
+
+    addStyle(
+      style = SpanStyle(color = dotColor),
+      start = 0,
+      end = mainString.length
+    )
+
+    addStyle(
+      style = SpanStyle(textDecoration = TextDecoration.Underline),
+      start = startIndex,
+      end = endIndex
+    )
+
+    addStringAnnotation(
+      tag = "URL",
+      annotation = context.getString(R.string.github_url),
+      start = startIndex,
+      end = endIndex
+    )
+  }
+
+  ClickableText(
+    modifier = modifier.padding(horizontal = 50.dp),
+    style = MaterialTheme.typography.h5.copy(fontSize = 12.sp, textAlign = TextAlign.Center, lineHeight = 14.sp),
+    text = annotatedString,
+    onClick = { index ->
+      annotatedString.getStringAnnotations("URL", index, index)
+        .firstOrNull()?.let { annotation ->
+          context.launchUrl(annotation.item)
+        }
+    })
 }
 
 @Preview
